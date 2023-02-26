49ac0d10c870556e857fcb33437484e9e85422aa
Abhay Garg <abhaycsemitrc2020@gmail.com>
2023-02-26 15:47:01 +0530

    first

--- src/app.js
+++ src/app.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const hbs = require('hbs');
 require("./db/conn");
 
 const port = process.env.PORT || 3000;
 
-const static_path = path.join(__dirname,"../public")
+const static_path = path.join(__dirname,"../public");
+const template_path = path.join(__dirname,"../templates/views");
+const partial_path = path.join(__dirname,"../templates/partials");
 // console.log(path.join(__dirname,"../public"))
 app.use(express.static(static_path));
 app.set("view engine","hbs");
+app.set("views",template_path);
+hbs.registerPartials( partial_path)
+
 
 app.get("/",(req,res)=>{
     res.render("index")
